5466aff88384ec47cae70d862093b02f5f3bb3a0
Yeshwanth Jaisamraj <yeshwanth.yeshu07@gmail.com>
2021-06-14 10:22:39 +0530

    Add files via upload

---
+++ Day1.py
@@ -0,0 +1,28 @@
+# assignment 1
+# print hello World
+
+print('Hello World')
+print("by Sriram D")
+
+#add two numbers
+a=10
+b=25
+c=a+b
+print(c)
+
+# find maximum
+
+a=int(input("enter numb1: "))
+b=int(input("enter numb2: "))
+if(a>b):
+    print("num1 is maximum")
+else:
+    print("num2 is maximum")
+
+#  OR 
+
+a=int(input("enter num1:"))
+b=int(input("enter num2:"))
+c=max(a,b)
+print("maximum value is:")
+print(c)
