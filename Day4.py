a57f789d37cd1260b7f935ee48349ec8356b7443
Yeshwanth Jaisamraj <yeshwanth.yeshu07@gmail.com>
2021-06-15 14:58:11 +0530

    Add files via upload

---
+++ Day4.py
@@ -0,0 +1,39 @@
+
+#Day 4: Python- Variables & Data types
+
+#-------------------------------------------------------------------------------------------
+#Q1. Create three variables (a,b,c) to same value of any integer & do the following
+
+#Assigning value
+a=b=c=1000
+print("Values of a=",a,"b=",b,"and c=",c)
+
+#a divide by 10
+print("Divide a by 10 ",a/10)
+
+
+#b multiply by 50
+print("Multiply b by 50",b*50)
+
+#Add c value by 60
+print("Add c value by 60",c+60)
+
+#-------------------------------------------------------------------------------------------
+#Q2. Create a string variable of 5 character and replace 3rd character with G
+
+#Creating string of 5 variables
+string = "aeiou"
+print(string)
+
+#replacing "i" with "G"
+print("Replacing G in string = ",string.replace("i","G"))
+
+#---------------------------------------------------------------------------------------------
+#Q3. Create two values (a,b) of int,float data type & convert the vise versa,
+
+#Creating values(a,b) of int, float
+a=50
+b=50.74
+
+print("Int to Float a: ",float(a))
+print("Float to Int b: ",int(b))
