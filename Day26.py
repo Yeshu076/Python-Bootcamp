47aa7e13ae21994f64f8a31580d08a5d7fa88538
Yeshwanth Jaisamraj <yeshwanth.yeshu07@gmail.com>
2021-07-11 18:06:36 +0530

    Add files via upload

---
+++ Day26.py
@@ -0,0 +1,15 @@
+
+#1)Enumerate a python list and try to print the counter with the list value
+
+list1= ['csk','rcb','mi','srh','dc']
+print(list(enumerate(list1)))
+
+
+print(enumerate(list1))
+
+#2Enumerate a python tuple and try to print the counter with the tuple value
+
+tuple1=('csk','rcb','mi','srh','dc')
+print(tuple(enumerate(tuple1)))
+
+print(enumerate(tuple1))
